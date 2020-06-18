1b713c9649fe5df8414bc46261d9798e8ee2891d
Sagar Patra <pydevsp@gmail.com>
2020-06-19 02:39:54 +0530

    added HelloApiView

--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -9,13 +9,18 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+
+####### use
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include ## include ====> use for app urls Configuration
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('profiles_api.urls')),
 ]
